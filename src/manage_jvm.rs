a70d222f2b5467b2addf2ec0fafb3546a4671f4a
EpicVon2468 <thomaschad0909@gmail.com>
2026-03-29 21:17:32 +1100

    architecture

--- src/manage_jvm.rs
+++ src/manage_jvm.rs
@@ -7,24 +7,46 @@ pub enum Op {
 		#[arg(short, long, default_value = "jbr")]
 		vendor: Vendor,
 
-		/// The requested JVM version.  Any valid string (or 'latest' for the latest available version)
-		// #[clap(default_value_t = String::from("latest"))]
-		version: String,
+		/// The architecture for the requested JVM
+		#[arg(short, long, default_value="system")]
+		arch: Arch,
 
 		/// The features for the requested JVM.  Note that not every vendor may support every feature, and some vendors may only offer features for certain versions or with incompatibilities with other features
 		#[arg(short, long)]
 		features: Vec<Features>,
+
+		/// The requested JVM version.  Any valid string (or 'latest' for the latest available version)
+		// #[clap(default_value_t = String::from("latest"))]
+		version: String,
 	},
 	Remove {
 	},
 }
 
 #[derive(ValueEnum, Clone)]
+pub enum Arch {
+	/// Automagically determine the system architecture
+	System,
+	/// 32-bit (i386)
+	X86,
+	/// 64-bit (amd64)
+	X64,
+	/// 32-bit AArch – https://developer.arm.com/Architectures/A32%20Instruction%20Set%20Architecture
+	Aarch32,
+	/// 64-bit AArch – https://developer.arm.com/Architectures/A64%20Instruction%20Set%20Architecture
+	Aarch64,
+	/// 32-bit RISC-V – https://riscv.org/
+	Riscv32,
+	/// 64-bit RISC-V – https://riscv.org/
+	Riscv64,
+}
+
+#[derive(ValueEnum, Clone)]
 pub enum Features {
-	/// Java Chromium Embedded Framework – https://github.com/chromiumembedded/java-cef/
-	JCEF,
 	/// Minimal (non-SDK/JDK) JVM (often referred to as a 'JRE').  If you don't know what this means & aren't a developer, you probably want this
 	MINIMAL,
+	/// Java Chromium Embedded Framework – https://github.com/chromiumembedded/java-cef/
+	JCEF,
 	/// MUSL libc support – https://musl.libc.org/
 	MUSL,
 }
