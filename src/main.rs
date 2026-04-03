904c77ad69cc2ef3461410da1cfad9908486ebb9
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-03 23:06:35 +1100

    never judge a book by its cover

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,13 @@ use crate::cmd_manage::cmd_manage;
 pub const FUJI_DIR: &str = if cfg!(windows) { "\\Program Files\\fuji" } else { "/opt/fuji" };
 
 fn main() {
+	if cfg!(windows) {
+		// Ever heard of "Never judge a book by is cover" ?
+		// It's about how you should judge based on the content of something, not what is on the outside
+		// Windows saw that, and said "Okay, but what if we made file extensions matter for execution instead?"
+		panic!("https://learn.microsoft.com/en-gb/windows/wsl/install");
+	};
+	#[allow(unreachable_code)]
 	// Doing this when trying to run the binary didn't work
 	unsafe {
 		set_var("RUST_BACKTRACE", "1");
