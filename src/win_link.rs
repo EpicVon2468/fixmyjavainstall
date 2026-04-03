f2a82774ad438c7dcf040ecab93d6160bbae10a4
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-03 21:55:42 +1100

    thanks I hate it

--- src/win_link.rs
+++ src/win_link.rs
@@ -14,7 +14,7 @@ pub fn win_link<P: AsRef<Path>>(bin_dir: P) -> Result<()> {
 		.read()
 		.write()
 		.open("Environment")
-		.unwrap_or_else(|| {
+		.unwrap_or_else(|_| {
 			windows_registry::CURRENT_USER
 				.options()
 				.read()
