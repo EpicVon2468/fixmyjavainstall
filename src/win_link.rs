e87b1ece834c7fdaabd54e19c0eb200d38951f4f
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-03 21:49:50 +1100

    WHAT DID I DO TO DESERVE THIS AAAAAAAAA

--- src/win_link.rs
+++ src/win_link.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 // Completely untested.  I don't even have the syntax highlighting to see if this works since I'm not on Windows.
 pub fn win_link<P: AsRef<Path>>(bin_dir: P) -> Result<()> {
 	let bin_dir: &Path = bin_dir.as_ref();
+	println!("Updating PATH with bin_dir {}", bin_dir.display());
 	// "To programmatically add or modify system environment variables, add them to the HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment registry key"
 	// https://learn.microsoft.com/en-gb/windows/win32/procthread/environment-variables
 	let key = windows_registry::LOCAL_MACHINE
@@ -13,9 +14,9 @@ pub fn win_link<P: AsRef<Path>>(bin_dir: P) -> Result<()> {
 		.read()
 		.write()
 		.open("Environment")
-		.unwrap();
-	let prev_path = key.get_string("PATH").unwrap();
+		.expect("Couldn't get Environment for HKEY_LOCAL_MACHINE");
+	let prev_path = key.get_string("PATH").expect("Couldn't get PATH");
 	// technically I'm supposed to broadcast a message about this, but uh... no.
-	key.set_string("PATH", format!("{prev_path};{}", bin_dir.display())).unwrap();
+	key.set_string("PATH", format!("{prev_path};{}", bin_dir.display())).expect("Couldn't set PATH");
 	Ok(())
 }
