5731bc2f6cf7c343bb71b2b9a0e627937c38ef94
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-03 18:31:36 +1100

    update win_link

--- src/win_link.rs
+++ src/win_link.rs
@@ -3,17 +3,19 @@ use std::io::Result;
 use std::path::Path;
 
 // https://stackoverflow.com/questions/79701236/what-is-the-recommended-way-to-append-a-path-to-windows-path-environment-vari
-// https://stackoverflow.com/questions/8358265/how-can-i-update-the-path-variable-permanently-from-the-windows-command-line
-// https://learn.microsoft.com/en-gb/windows/win32/procthread/environment-variables
 // Completely untested.  I don't even have the syntax highlighting to see if this works since I'm not on Windows.
 pub fn win_link<P: AsRef<Path>>(bin_dir: P) -> Result<()> {
-	let key = windows_registry::CURRENT_USER
+	let bin_dir: &Path = bin_dir.as_ref();
+	// "To programmatically add or modify system environment variables, add them to the HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment registry key"
+	// https://learn.microsoft.com/en-gb/windows/win32/procthread/environment-variables
+	let key = windows_registry::LOCAL_MACHINE
 		.options()
 		.read()
 		.write()
 		.open("Environment")
 		.unwrap();
 	let prev_path = key.get_string("PATH").unwrap();
-	key.set_string("PATH", format!("{prev_path};{}", bin_dir.as_ref().display()));
+	// technically I'm supposed to broadcast a message about this, but uh... no.
+	key.set_string("PATH", format!("{prev_path};{}", bin_dir.display()));
 	Ok(())
 }
