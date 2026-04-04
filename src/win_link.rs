798e49ff72945bd7dcd8d32112fed84b22e30aa6
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-04 18:45:03 +1100

    I hate modifying this file

--- src/win_link.rs
+++ src/win_link.rs
@@ -1,32 +1,39 @@
 #![cfg(windows)]
-use std::io::Result;
+
 use std::path::Path;
+
+macro_rules! open_env {
+    ($holder:ident) => {
+		windows_registry::$holder
+			.option()
+			.read()
+			.write()
+			.open("Environment")
+	};
+}
 
 // https://stackoverflow.com/questions/79701236/what-is-the-recommended-way-to-append-a-path-to-windows-path-environment-vari
 // Completely untested.  I don't even have the syntax highlighting to see if this works since I'm not on Windows.
 /// Appends `bin_dir` to the `PATH` environment variable.
 ///
 /// Initially, this will attempt to modify the `PATH` of `HKEY_LOCAL_MACHINE`, however if that fails it will use `HKEY_CURRENT_USER` as a fallback.
-pub fn win_link<P: AsRef<Path>>(bin_dir: P) -> Result<()> {
+pub fn win_link<P: AsRef<Path>>(bin_dir: P) -> std::io::Result<()> {
 	let bin_dir: &Path = bin_dir.as_ref();
 	println!("Updating PATH with bin_dir {}", bin_dir.display());
 	// "To programmatically add or modify system environment variables, add them to the HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment registry key"
 	// https://learn.microsoft.com/en-gb/windows/win32/procthread/environment-variables
-	let key = windows_registry::LOCAL_MACHINE
-		.options()
-		.read()
-		.write()
-		.open("Environment")
+	let environment = open_env!(LOCAL_MACHINE)
 		.unwrap_or_else(|_| {
-			windows_registry::CURRENT_USER
-				.options()
-				.read()
-				.write()
-				.open("Environment")
+			open_env!(CURRENT_USER)
 				.expect("Couldn't get Environment for HKEY_LOCAL_MACHINE or HKEY_CURRENT_USER")
 		});
-	let prev_path = key.get_string("PATH").expect("Couldn't get PATH");
+	let prev_path = environment.get_string("PATH").expect("Couldn't get PATH environment variable!");
 	// technically I'm supposed to broadcast a message about this, but uh... no.
-	key.set_string("PATH", format!("{};{prev_path}", bin_dir.display())).expect("Couldn't set PATH");
+	environment
+		.set_string(
+			"PATH",
+			format!("{};{prev_path}", bin_dir.display())
+		)
+		.expect("Couldn't set PATH environment variable!");
 	Ok(())
 }
