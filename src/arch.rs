3a676d85daba60048962117e847feea86cccf0aa
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-12 12:42:41 +1000

    add aliases for architectures and update doc comments to point to technical specifications

--- src/arch.rs
+++ src/arch.rs
@@ -9,11 +9,14 @@ use clap::builder::OsStr;
 /// An enumeration of CPU architectures
 #[derive(ValueEnum, Clone)]
 pub enum Arch {
-	/// 64-bit (amd64)
+	/// 64-bit extensions for x86 – <https://docs.amd.com/v/u/en-US/40332_4.09_APM_PUB>
+	#[clap(aliases = vec!["amd", "amd64", "intel", "intel64", "x86_64", "x86-64"])]
 	X64,
-	/// 64-bit AArch (arm64) – <https://developer.arm.com/Architectures/A64%20Instruction%20Set%20Architecture/>
+	/// 64-bit ARM – <https://developer.arm.com/Architectures/A64%20Instruction%20Set%20Architecture/>
+	#[clap(aliases = vec!["arm", "arm64", "aarch"])]
 	Aarch64,
-	/// 64-bit RISC-V – <https://riscv.org/>
+	/// 64-bit RISC-V – <https://docs.riscv.org/reference/isa/index.html>
+	#[clap(aliases = vec!["riscv", "risc-v"])]
 	Riscv64,
 }
 
