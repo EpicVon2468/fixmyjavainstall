42cd3d6818d3e0854d9b96e5b84a103dc7535a90
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-23 20:15:29 +1000

    v0.3.2: "So help me cargo-dist I will drop you right now"

--- src/cli.rs
+++ src/cli.rs
@@ -10,7 +10,7 @@ use crate::LINK_DIR;
 #[derive(Parser)]
 #[command(
 	version,
-	long_version = "0.3.1 – \"It's me.  John Progress Bar.\"",
+	long_version = "0.3.2 – \"So help me cargo-dist I will drop you right now\"",
 	author
 )]
 pub struct FujiArgs {
