becdb3a84f2e088ca42a52f1f43ac172c8dca28e
EpicVon2468 <thomaschad0909@gmail.com>
2026-04-26 10:45:01 +1000

    store previous event, (neo)vi(m) exit keybind

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -18,6 +18,7 @@ use crate::tui::tab::Tab;
 pub struct FujiApp {
 	pub page: Cell<Box<dyn Page>>,
 	pub event: Option<Event>,
+	pub prev_event: Option<Event>,
 }
 
 impl FujiApp {
@@ -25,7 +26,24 @@ impl FujiApp {
 		Self {
 			page: Cell::new(Box::new(JVMPage { tab: Tab::Foo })),
 			event: None,
+			prev_event: None,
 		}
+	}
+
+	pub fn key_pressed(&self, prev: bool, validate: &dyn Fn(KeyCode) -> bool) -> bool {
+		let event = if prev { &self.prev_event } else { &self.event }.as_ref();
+		if let Some(&Event::Key(key_event)) = event {
+			validate(key_event.code)
+		} else {
+			false
+		}
+	}
+
+	pub fn should_exit(&self) -> bool {
+		self.key_pressed(false, &|key| {
+			self.key_pressed(true, &|key| matches!(key, KeyCode::Char(':')))
+				&& matches!(key, KeyCode::Char('q'))
+		})
 	}
 
 	pub fn main(&mut self, mut terminal: DefaultTerminal) -> Result<()> {
@@ -33,17 +51,14 @@ impl FujiApp {
 			terminal.draw(|frame: &mut Frame| self.render(frame))?;
 			if !poll(Duration::from_millis(0))? {
 				if self.event.is_some() {
-					self.event.take();
+					self.prev_event = self.event.take();
 				};
 				continue;
 			};
-			let event: Event = read()?;
-			if let Event::Key(key_event) = event
-				&& key_event.code == KeyCode::Char('q')
-			{
+			self.event.replace(read()?);
+			if self.should_exit() {
 				break Ok(());
 			};
-			self.event.replace(event);
 		}
 	}
 
